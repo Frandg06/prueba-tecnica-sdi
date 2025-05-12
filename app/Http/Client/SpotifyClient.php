107c6a62ca26309b855834d0b437dba332a43155
frandg06 <fdiez86@gmail.com>
2025-05-13 00:49:50 +0200

    refactor: Mejorar la estructura del código en SpotifyClient
    - Se reorganizó el código para mejorar la legibilidad.
    - Se mantuvo la funcionalidad existente mientras se optimizó la estructura.
    - No se requieren notas adicionales para la revisión.

--- app/Http/Client/SpotifyClient.php
+++ app/Http/Client/SpotifyClient.php
@@ -10,58 +10,55 @@ use Illuminate\Support\Facades\Log;
 
 /**
  * @var string TOKEN_KEY Clave usada para almacenar el token de Spotify
+ *
  * @property string $apiBaseUrl
  * @property string $authUrl
  */
 class SpotifyClient
 {
-  private const TOKEN_KEY = "spotify-token";
-  private string $apiBaseUrl;
-  private string $authUrl;
+    private const TOKEN_KEY = 'spotify-token';
 
-  public function __construct()
-  {
-    $this->apiBaseUrl = config('spotify.api_url');
-    $this->authUrl = config('spotify.spotify_auth_url');
-  }
+    private string $apiBaseUrl;
 
-  /**
-   * @param string $endpoint
-   * @param array $params
-   * @return array
-   */
-  public function get(string $endpoint, array $params = []): array
-  {
+    private string $authUrl;
 
-    $response = Http::withToken($this->getToken())
-      ->get("{$this->apiBaseUrl}/{$endpoint}", $params)->json();
-
-    if (isset($response['error'])) {
-      throw new SpotifyException($response['error']['message'], $response['error']['status']);
+    public function __construct()
+    {
+        $this->apiBaseUrl = config('spotify.api_url');
+        $this->authUrl = config('spotify.spotify_auth_url');
     }
 
-    return $response;
-  }
+    public function get(string $endpoint, array $params = []): array
+    {
 
-  /**
-   * @return string
-   * @throws ApiException
-   */
-  private function getToken(): string
-  {
-    try {
-      return Cache::remember(self::TOKEN_KEY, 3500, function () {
-        $response = Http::asForm()->post($this->authUrl, [
-          'grant_type'    => 'client_credentials',
-          'client_id'     => config('spotify.client_id'),
-          'client_secret' => config('spotify.client_secret'),
-        ])->throw()->json();
+        $response = Http::withToken($this->getToken())
+            ->get("{$this->apiBaseUrl}/{$endpoint}", $params)->json();
 
-        return $response['access_token'];
-      });
-    } catch (\Exception $e) {
-      Log::error($e->getMessage());
-      throw new ApiException(__('i18n.spotify_token_ko'), 500);
+        if (isset($response['error'])) {
+            throw new SpotifyException($response['error']['message'], $response['error']['status']);
+        }
+
+        return $response;
     }
-  }
+
+    /**
+     * @throws ApiException
+     */
+    private function getToken(): string
+    {
+        try {
+            return Cache::remember(self::TOKEN_KEY, 3500, function () {
+                $response = Http::asForm()->post($this->authUrl, [
+                    'grant_type' => 'client_credentials',
+                    'client_id' => config('spotify.client_id'),
+                    'client_secret' => config('spotify.client_secret'),
+                ])->throw()->json();
+
+                return $response['access_token'];
+            });
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            throw new ApiException(__('i18n.spotify_token_ko'), 500);
+        }
+    }
 }
