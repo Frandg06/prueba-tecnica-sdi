ad58772dcbc5a5066475320b2cc375e4cc2db225
frandg06 <fdiez86@gmail.com>
2025-05-13 00:50:38 +0200

    refactor: Mejorar la organización y formato del código en app.php
    - Se ajustó el formato de las rutas para mayor claridad.
    - Se añadió una coma faltante en la definición del alias de middleware.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,19 +6,18 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Client\RequestException;
 use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'i18n' => \App\Http\Middleware\i18nMiddleware::class
+            'i18n' => \App\Http\Middleware\i18nMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
