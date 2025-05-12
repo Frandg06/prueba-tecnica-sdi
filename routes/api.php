2a2fb8f58d77f1968a201011ddca3b95765dff87
frandg06 <fdiez86@gmail.com>
2025-05-13 00:55:52 +0200

    test: Probar el workflow para formatear el codigo

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,11 @@ use App\Http\Controllers\SpotifyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+
+
+
 
 Route::prefix('v1')->middleware('i18n')->group(function () {
     Route::prefix('auth')->group(function () {
