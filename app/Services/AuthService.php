35674baa3b828e0976182080a2fa7e9a66b03447
frandg06 <fdiez86@gmail.com>
2025-05-13 00:50:28 +0200

    refactor: Mejorar la legibilidad y organización del código en AuthService
    - Se reorganizó la estructura de los métodos para mejorar la claridad.
    - Se eliminaron comentarios innecesarios y se ajustó la indentación.
    - Se mantuvo la funcionalidad existente sin cambios.

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -12,79 +12,73 @@ use Illuminate\Support\Facades\Log;
 
 class AuthService
 {
+    /**
+     * @throws Exception
+     */
+    public function register(array $data): array
+    {
+        DB::beginTransaction();
+        try {
 
-  /**
-   * @param array $data
-   * @throws Exception
-   * @return array
-   */
-  public function register(array $data): array
-  {
-    DB::beginTransaction();
-    try {
+            $user = User::create($data);
 
-      $user = User::create($data);
+            $user->createToken('Spotify')->plainTextToken;
 
-      $user->createToken('Spotify')->plainTextToken;
+            DB::commit();
 
-      DB::commit();
-      return [
-        'token' => $user->createToken('Spotify')->plainTextToken,
-        'user' => $user->toResource(),
-      ];
-    } catch (\Exception $e) {
-      DB::rollBack();
-      Log::error($e->getMessage());
-      throw new ApiException(__('i18n.register_user_ko'), 500);
+            return [
+                'token' => $user->createToken('Spotify')->plainTextToken,
+                'user' => $user->toResource(),
+            ];
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            throw new ApiException(__('i18n.register_user_ko'), 500);
+        }
     }
-  }
 
-  /**
-   * @param array $data
-   * @return array
-   * @throws Exception
-   */
-  public function login(array $data): array
-  {
-    DB::beginTransaction();
-    try {
+    /**
+     * @throws Exception
+     */
+    public function login(array $data): array
+    {
+        DB::beginTransaction();
+        try {
 
-      $user = User::where('email', $data['email'])->first();
+            $user = User::where('email', $data['email'])->first();
 
-      if (!Hash::check($data['password'], $user->password)) {
-        throw new ApiException(__('i18n.credentials_invalid'), 500);
-      }
+            if (! Hash::check($data['password'], $user->password)) {
+                throw new ApiException(__('i18n.credentials_invalid'), 500);
+            }
 
-      $user->tokens()->delete();
+            $user->tokens()->delete();
 
-      DB::commit();
-      return [
-        'token' => $user->createToken('Spotify')->plainTextToken,
-        'user' => $user->toResource(),
-      ];
-    } catch (\Exception $e) {
-      DB::rollBack();
-      Log::error($e->getMessage());
-      throw new ApiException(__('i18n.login_user_ko'), 500);
+            DB::commit();
+
+            return [
+                'token' => $user->createToken('Spotify')->plainTextToken,
+                'user' => $user->toResource(),
+            ];
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            throw new ApiException(__('i18n.login_user_ko'), 500);
+        }
     }
-  }
 
-
-  /**
-   * @param User $user
-   * @throws Exception
-   * @return void
-   */
-  public function logout(User $user): void
-  {
-    DB::beginTransaction();
-    try {
-      $user->tokens()->delete();
-      DB::commit();
-    } catch (\Exception $e) {
-      DB::rollBack();
-      Log::error($e->getMessage());
-      throw new ApiException(__('i18n.logout_ko'), 500);
+    /**
+     * @throws Exception
+     */
+    public function logout(User $user): void
+    {
+        DB::beginTransaction();
+        try {
+            $user->tokens()->delete();
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            throw new ApiException(__('i18n.logout_ko'), 500);
+        }
     }
-  }
 }
