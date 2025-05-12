5d8becf4cc7421905b4697acd6a5937a3c010a74
frandg06 <fdiez86@gmail.com>
2025-05-13 00:50:50 +0200

    refactor: Mejorar la indentación en el archivo i18n.php

--- lang/en/i18n.php
+++ lang/en/i18n.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-  'logout' => 'The session has been successfully closed',
-  'register_user_ko' => 'There was an error registering the user',
-  'login_user_ko' => 'There was an error logging in the user',
-  'credentials_invalid' => 'The login credentials are invalid',
-  'logout_ko' => 'There was an error closing the session',
-  'unexpected_error' => 'There was an unexpected error',
-  'validation_error' => 'There was an error validating the data',
-  'authentication_error' => 'There was an error authenticating the user',
-  'spotify_token_ko' => 'There was an error getting the Spotify token',
-  'spotify_error' => 'There was an error in the Spotify API',
+    'logout' => 'The session has been successfully closed',
+    'register_user_ko' => 'There was an error registering the user',
+    'login_user_ko' => 'There was an error logging in the user',
+    'credentials_invalid' => 'The login credentials are invalid',
+    'logout_ko' => 'There was an error closing the session',
+    'unexpected_error' => 'There was an unexpected error',
+    'validation_error' => 'There was an error validating the data',
+    'authentication_error' => 'There was an error authenticating the user',
+    'spotify_token_ko' => 'There was an error getting the Spotify token',
+    'spotify_error' => 'There was an error in the Spotify API',
 ];
