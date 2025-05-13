d1379c961b244c5b242cfdf739f67559cf544fe3
frandg06 <fdiez86@gmail.com>
2025-05-13 14:54:15 +0200

    style: Mejorar formato de código en SpotifyService - Se ajustó el espaciado en las llamadas a métodos para mayor claridad. - Se añadieron comentarios de tipo para mejorar la documentación del código.

--- app/Services/SpotifyService.php
+++ app/Services/SpotifyService.php
@@ -14,6 +14,10 @@ class SpotifyService
 {
     public function __construct(private readonly SpotifyClient $client) {}
 
+    /**
+     * @param SearchDto $data
+     * @return array
+     */
     public function search(SearchDto $data): array
     {
         $response = $this->client->get('search', $data->toArray());
@@ -21,30 +25,49 @@ class SpotifyService
         return $response;
     }
 
+    /**
+     * @param string $id
+     * @param string $market
+     * @return array
+     */
     public function album(string $id, string $market = ''): array
     {
-        $response = $this->client->get('albums/'.$id, ['market' => $market]);
+        $response = $this->client->get('albums/' . $id, ['market' => $market]);
 
         return $response;
     }
 
+    /**
+     * @param string $id
+     * @param string $market
+     * @return array    
+     */
     public function track(string $id, string $market = ''): array
     {
-        $response = $this->client->get('tracks/'.$id, ['market' => $market]);
+        $response = $this->client->get('tracks/' . $id, ['market' => $market]);
 
         return $response;
     }
 
+    /**
+     * @param string $id
+     * @return array
+     */
     public function artist(string $id): array
     {
-        $response = $this->client->get('artists/'.$id);
+        $response = $this->client->get('artists/' . $id);
 
         return $response;
     }
 
+    /**
+     * @param string $id
+     * @param string $market
+     * @return array
+     */
     public function topTracks(string $id, string $market = ''): array
     {
-        $response = $this->client->get('artists/'.$id.'/top-tracks', ['market' => $market]);
+        $response = $this->client->get('artists/' . $id . '/top-tracks', ['market' => $market]);
 
         return $response;
     }
