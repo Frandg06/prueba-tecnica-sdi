20af420dd00c1c6497f5946848b7adee7fc8c8ff
frandg06 <fdiez86@gmail.com>
2025-05-13 14:38:25 +0200

    feat: Agregar archivo api.json y ruta para documentación de API
    - Se creó el archivo api.json que define la especificación OpenAPI.
    - Se añadió una nueva ruta en web.php para servir la documentación de la API.
    - Esto permite a los desarrolladores acceder a la documentación de la API de manera más fácil.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Dedoc\Scramble\Http\Middleware\RestrictedDocsAccess;
+use Dedoc\Scramble\Scramble;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/docs', function () {
+    return view('scramble::docs', [
+        'spec' => file_get_contents(base_path('api.json')),
+        'config' => Scramble::getGeneratorConfig('default'),
+    ]);
+})->middleware(Scramble::getGeneratorConfig('default')->get('middleware', [RestrictedDocsAccess::class]));
